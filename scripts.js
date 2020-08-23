9045aea9dacf83505d7a3cd1e4865fc04e944e21
godigitinsurance <59437973+godigitinsurance@users.noreply.github.com>
2020-08-23 18:38:28 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,13 @@
+$("#customerName").text("Gurwinder Singh");
+$("#policyNumber").text("DOI7262731");
+$("#startDate").text("24-Aug-2020");
+$("#endDate").text("23-Aug-2021");
+
+
+$("#type").text("Two Wheeler");
+$("#registrationNumber").text("PB08DR5986");
+$("#make").text("HONDA");
+$("#model").text("LIVO");
+$("#vehicalSubType").text("ELECTRIC START");
+$("#engineNo").text("JC71ET1013291");
+$("#chassis").text("ME4JC716DHT005194");
